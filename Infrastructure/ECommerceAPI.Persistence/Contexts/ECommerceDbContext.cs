c49cc4955108fe70e556c328ec314500a5119725
Arzu Teymurova <arzuteymurova95@gmail.com>
2024-03-29 17:49:51 +0400

    Add project files.

---
+++ Infrastructure/ECommerceAPI.Persistence/Contexts/ECommerceDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Persistence.Contexts
+{
+    public class ECommerceDbContext:DbContext
+    {
+        public ECommerceDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceAPI.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            //services.AddSingleton<IProductService, ProductService>();
+
+            services.AddDbContext<ECommerceDbContext>(options => options.UseSqlServer());
+        }
+    }
+}
